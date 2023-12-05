191c9a31c8d16094e313cb4781cb3e8ef26f409c
adam-iqbal <131178364+adam-iqbal@users.noreply.github.com>
2023-12-05 16:42:16 +0000

    Update ambulatory_testing.R

--- testing/ambulatory_testing.R
+++ testing/ambulatory_testing.R
@@ -1,7 +1,12 @@
+# Required packages: ssmrob (for dataset), sampleSelection (for full model and stepwise), MASS (for spike-slab and ALASSO), rtruncnorm (for spike-slab), numDeriv (for spike-slab)
+# Additionally requires all the functions in the methods files to be loaded (gibbs_spike_slab, stepwise_ssel, all the functions in alasso.R and utils.R)
+
+# Random seed for reproducibility in spike-and-slab sampler
+set.seed(1234)
+
+# Constructing subset of data for use in simulation study
 library(ssmrob)
 data(MEPS2001)
-
-set.seed(1234)
 dat <- MEPS2001[,-c(6,7,11)]
 y <- dat[,6]
 w <- dat[,-c(6,8)]
@@ -14,6 +19,7 @@ n <- nrow(x)
 p <- ncol(w)
 q <- ncol(x)
 
+# Parameters for spike-and-slab priors
 n_samp <- 50000
 burn_in <- 5000
 tau_0_alpha <- 1/sqrt(p*n)
@@ -28,6 +34,7 @@ alpha_slab <- "normal"
 beta_spike <- "normal"
 beta_slab <- "normal"
 
+# Full model
 s_formula <- s ~ educ + age + income + female + totchr + blhisp + ins
 y_formula <- y ~ educ + age + female + totchr + blhisp + ins
 ssel <- tryCatch(selection(s_formula, 
@@ -37,6 +44,7 @@ ssel <- tryCatch(selection(s_formula,
 
 amb_ssel <- ssel
 
+# Initial values for spike-and-slab
 if(sum(is.na(ssel))==0){
   init_alpha = ssel$estimate[1:(p+1)]
   init_beta = ssel$estimate[(p+2):(p+q+2)]
@@ -54,7 +62,8 @@ if(sum(is.na(ssel))==0){
   init_gamma_alpha = rep(0,p)
   init_gamma_beta = rep(0,q)
 }
-current_time = Sys.time()
+
+# Spike-and-slab run
 amb_gibbs = gibbs_spike_slab(n_samp,y,x,w,burn_in=burn_in,
                               alpha_spike = alpha_spike,
                               alpha_slab = alpha_slab,
@@ -76,17 +85,16 @@ amb_gibbs = gibbs_spike_slab(n_samp,y,x,w,burn_in=burn_in,
                              var_scaling=FALSE,
                             weak_intercept=TRUE)
 
+# Adaptive LASSO - needs 0 instead of NA in the outcome, and requires a different selection of lambda than default
 test_dat_no_na <- test_dat
 test_dat_no_na$y <- ifelse(is.na(test_dat$y),0,test_dat$y)
-
 lambda.max=10
 lambda.min=1e-4
 n.lambda=100
 lambda = exp(seq(log(lambda.max),log(lambda.min),length.out=n.lambda))
-
 amb_lasso <- Heckman_lsa_scaled(s_formula, y_formula, data = test_dat_no_na,
   penalty="ALASSO", crit="bic",lambda=lambda)
-gc(reset = TRUE)
 
+# Forwards selection
 ssel_null <- selection(s~1,y~1,data=test_dat,start=c(0,0,1,0))
 amb_stepwise <- stepwise_ssel(ssel_null,path="add",S_vars=colnames(w),O_vars=colnames(x))
